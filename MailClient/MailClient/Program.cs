40f3ae0b5b5258f6710775e59b0109cc3acd7ddb
my0sotis <35601060+my0sotis@users.noreply.github.com>
2020-03-21 16:17:36 +0800

    C# Project

---
+++ MailClient/MailClient/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MailClient.Utils;
+
+namespace MailClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = "Hello World!";
+            Console.WriteLine(Base64.Base64Encode(x));
+            x = Base64.Base64Encode(x);
+            Console.WriteLine(Base64.Base64Decode(x));
+        }
+    }
+}
