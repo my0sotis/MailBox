d549902ac791e50f3e796506c1e8128306d96bd1
my0sotis <35601060+my0sotis@users.noreply.github.com>
2020-04-09 20:14:43 +0800

    补充内容

---
+++ MailClient/src/main/java/com/mail/MailClient/entity/ReceivedMail.java
@@ -0,0 +1,19 @@
+package com.mail.MailClient.entity;
+
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+public class ReceivedMail {
+    private int getMonth(String str) {
+        return 0;
+    }
+
+    public void processDateFormat(String str) {
+        String[] fragment = str.split(" ");
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
